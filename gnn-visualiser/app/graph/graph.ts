9d64f60706e5eba4c7edfe7c97406faae3827609
hometue <hometue.lee@gmail.com>
2024-02-22 18:03:40 +0800

    Finalize graph implementation

--- gnn-visualiser/app/graph/graph.ts
+++ gnn-visualiser/app/graph/graph.ts
@@ -1,20 +1,45 @@
 type GraphNode = {
-    nodeFeature: Number
+    nodeFeature: number
 }
 
 class Graph {
-    adjMatrix: Number[][] = [[]]; // Simple version, 0 means not connected, non zero is connected
-    nodeFeatures: Number[] = [];
-
-    constructor(){
-    }
+    adjMatrix: number[][] = []; // Simple version, 0 means not connected, non zero is connected
+    nodeFeatures: number[] = [];
 
     addNode(){
         this.adjMatrix.forEach((row) => {
-            row.push(0);
+            //row.push(0); testing
+            row.push(this.adjMatrix.length);
         });
-        let newRow: Number[] = new Array<Number>(this.adjMatrix.length + 1);
-        newRow.fill(0, 0, this.adjMatrix.length + 1)
-        this.adjMatrix . push (newRow);
+        let newRow: number[] = new Array<number>(this.adjMatrix.length + 1);
+        newRow.fill(0, 0, this.adjMatrix.length + 1);
+        this.adjMatrix.push(newRow);
+
+        // Add nodeFeatures
+
+        this.nodeFeatures.push(0);
+    }
+
+    removeNode(id: number){
+        if(id >= this.adjMatrix.length){
+            // Check out of bound, early return
+            return;
+        }
+        this.adjMatrix.splice(id, 1);
+        this.adjMatrix.forEach((row) => {
+            row.splice(id, 1);
+        })
+    }
+
+    printNodes(){
+        console.log(this.adjMatrix);
+    }
+
+    setNodeFeature(id: number){
+
+    }
+
+    setNodesConnection(firstNodeId: number, secondNodeId: number){
+        
     }
 }
