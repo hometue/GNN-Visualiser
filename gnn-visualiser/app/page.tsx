4b9c488c987b8f8efab7430bf9bf643877032599
hometue <hometue.lee@gmail.com>
2024-03-05 15:03:11 +0800

    Updating main layout

--- gnn-visualiser/app/page.tsx
+++ gnn-visualiser/app/page.tsx
@@ -2,9 +2,13 @@
 
 import Image from "next/image";
 import GraphView from "./graph/graphView";
+import AdjGraphView from "./graph/adjGraphView";
 
 export default function Test() {
   return (
-    <GraphView />
+    <div style={{display: "flex"}}>
+      <div style={{flex: 1}}><AdjGraphView /></div>
+      <div style={{flex: 1}}><GraphView /></div>
+    </div>
   )
 }
