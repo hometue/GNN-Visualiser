1613ab52dc69e3354c8f71524fb378ee8733f332
hometue <hometue.lee@gmail.com>
2024-03-05 14:35:51 +0800

    Formatting

--- gnn-visualiser/app/graph/graphView.tsx
+++ gnn-visualiser/app/graph/graphView.tsx
@@ -3,52 +3,52 @@ import {Graph} from "./graph";
 import cytoscape from 'cytoscape';
 
 function graphToCyto(graph: Graph){
-    let id = 0;
-    const graphData: {data: {}}[] = [];
-    graph.adjMatrix.forEach((node, index) => {
-        const nodeData = {data: {id: index.toString()}};
-        graphData.push(nodeData);
-        node.forEach((edge, edgeNode) => {
-            if(edge != 0){
-                const edgeData = {data: {id: index.toString().concat('e'.concat(edgeNode.toString())),
-                                        source: index.toString(),
-                                        target: edgeNode.toString()
-                }};
-                graphData.push(edgeData);
-            }
-        })
-    })
-    return graphData;
+	let id = 0;
+	const graphData: {data: {}}[] = [];
+	graph.adjMatrix.forEach((node, index) => {
+		const nodeData = {data: {id: index.toString()}};
+		graphData.push(nodeData);
+		node.forEach((edge, edgeNode) => {
+			if(edge != 0){
+				const edgeData = {data: {id: index.toString().concat('e'.concat(edgeNode.toString())),
+										source: index.toString(),
+										target: edgeNode.toString()
+				}};
+				graphData.push(edgeData);
+			}
+		})
+	})
+	return graphData;
 }
 
 
 export default function GraphView() {
-    const graphRef = useRef(null)
-    const [graph, setGraph] = useState(() => {
-        const graph = new Graph();
-        graph.addNode();
-        graph.addNode();
-        graph.addNode();
-        return graph;
-    });
-    useEffect(()=> {
-        let cy = cytoscape({
-            container: graphRef.current,
-            elements: graphToCyto(graph),
-            layout: {name: 'circle'},
-            style: [
-                {
-                  selector: 'node',
-                  style: {
-                    'label': 'data(id)'
-                  }
-                }
-              ]
-        });
-    }, [])
-    
+	const graphRef = useRef(null)
+	const [graph, setGraph] = useState(() => {
+		const graph = new Graph();
+		graph.addNode();
+		graph.addNode();
+		graph.addNode();
+		return graph;
+	});
+	useEffect(()=> {
+		let cy = cytoscape({
+			container: graphRef.current,
+			elements: graphToCyto(graph),
+			layout: {name: 'circle'},
+			style: [
+				{
+				selector: 'node',
+				style: {
+					'label': 'data(id)'
+				}
+				}
+			]
+		});
+	}, [])
+	
 
-    return (
-        <div style={{width: '100%', height: '80vh'}} ref={graphRef}></div>
-    )
+	return (
+		<div style={{width: '100%', height: '80vh'}} ref={graphRef}></div>
+	)
 }
