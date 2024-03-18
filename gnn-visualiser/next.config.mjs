23fca28e36133aca65884458878939b626800f1a
hometue <hometue.lee@gmail.com>
2024-03-18 19:31:49 +0800

    Fix dev server

--- gnn-visualiser/next.config.mjs
+++ gnn-visualiser/next.config.mjs
@@ -1,7 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-	basePath: "/GNN-Visualiser",
-	output: "export"
-};
 
-export default nextConfig;
+import { PHASE_PRODUCTION_BUILD } from 'next/dist/shared/lib/constants.js';
+
+
+
+// @ts-check
+ 
+export default (phase, { defaultConfig }) => {
+	/**
+	 * @type {import('next').NextConfig}
+	 */
+	if (phase === PHASE_PRODUCTION_BUILD) {
+		const nextConfig = {
+			//basePath: "/GNN-Visualiser",
+			output: "export"
+		};
+		return nextConfig
+	}
+	else{
+		const nextConfig = {
+			//basePath: "/GNN-Visualiser",
+			output: "export"
+		};
+		return nextConfig
+	}
+}
